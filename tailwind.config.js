ccc752053d9bfddb16375705db9618f760534386
BarbatosSifiruz <makotohishigami@gmail.com>
2023-09-27 19:35:47 -0300

    Fix problem whit styles in line 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/{html,js}"],
+    content: ["./src/index.html", "./src/js/main.js"],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: ["tailwindcss"],
 };
